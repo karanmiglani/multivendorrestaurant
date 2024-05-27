b79892337a8d38b7c8b89317a59e53408e1d8711
karanmiglani <karanmiglani03@gmail.com>
2024-05-27 17:42:04 +0530

    Login/logot completed with permissions

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path('register-user/',views.registerUser , name = 'register-user'),
-    path('register-vendor/',views.registerVendor , name = 'register-vendor')
+    path('register-vendor/',views.registerVendor , name = 'register-vendor'),
+    path('login/' , views.login , name = 'login'),
+    path('logout/' , views.logout , name='logout'),
+    path('myAccount/' , views.myAccount , name = 'myAccount'),
+    path('customerDashboard/', views.customerDashboard, name='customerDashboard'),
+    path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard')
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,34 @@ from django.shortcuts import render , redirect
 from vendor.forms import VendorForm
 from .forms import UserForm
 from .models import User, UserProfile
-from django.contrib import messages
+from django.contrib import messages , auth
+from django.contrib.auth.decorators import login_required , user_passes_test
+from .utils import detectUser
+from django.core.exceptions import PermissionDenied
 # Create your views here.
+
+# restrict vendor from accessing the cutomer page
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
+
+
+# restrict cutomer from accessing the vendor page
+
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
+
+
 def registerUser(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.add_message(request , messages.WARNING , 'You are already registered!')
+        return redirect('dashboard')
+    elif request.method == 'POST':
         form = UserForm(request.POST)
         print(request.POST)
         if form.is_valid():
@@ -35,7 +59,10 @@ def registerUser(request):
 
 
 def registerVendor(request):
-    if request.method == "POST":
+    if request.user.is_authenticated:
+        messages.add_message(request , messages.WARNING , 'You are already registered!')
+        return redirect('dashboard')
+    elif request.method == "POST":
         userForm = UserForm(request.POST)
         form = VendorForm(request.POST , request.FILES)
         if userForm.is_valid() and form.is_valid():
@@ -66,3 +93,45 @@ def registerVendor(request):
         form = VendorForm()
     context = {'form' : form,'user_form' :userForm}
     return render(request , 'accounts/register_vendor.html',context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        messages.add_message(request , messages.WARNING , 'You are already logged in')
+        return redirect('myAccount')
+    elif request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email = email , password = password)
+
+        if user is not None:
+            auth.login(request , user)
+            messages.add_message(request , messages.SUCCESS , 'You are now logged in')
+            return redirect('myAccount')
+        else:
+            messages.add_message(request , messages.ERROR , 'Invalid login credentials')
+            return redirect('login')
+    return render(request , 'accounts/login.html')
+
+def logout(request):
+    auth.logout(request)
+    messages.add_message(request , messages.INFO , 'You are logged out.')
+    return redirect('login')
+
+
+@login_required(login_url = 'login')
+def myAccount(request):
+    user = request.user
+    redirectUrl = detectUser(user)
+    return redirect(redirectUrl)
+
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def customerDashboard(request):
+    return render(request , 'accounts/customer_dashboard.html')
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def vendorDashboard(request):
+    return render(request , 'accounts/vendor_dashboard.html')
